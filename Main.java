2c6b682b23c5bb8462c8fd27e8980c6f5bce7a87
Kyle McDowell <kymcd21@icstudents.org>
2021-02-17 16:16:31 +0000

    almost working

--- Main.java
+++ Main.java
@@ -1,8 +1,29 @@
+import java.io.*;
+import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) throws Exception {
+    Scanner fileNameInput = new Scanner(System.in);
+    System.out.println("Enter the name of the file you want to output to.");
+    String fileName = fileNameInput.nextLine();
+    int fileLines=0;
+    do {
+      System.out.println("enter the amount of lines you want to input");
+      fileLines = fileNameInput.nextInt();
+      if (fileLines != Integer) {
+        throw new IllegalArgumentException("Please enter an integer.");
+      } else {
+        break;
+      }
+    } while (!fileNameInput.hasNextInt());
 
+    PrintWriter fileOutput = new PrintWriter(fileName);
+    for (int lineNo = 0; lineNo < fileLines; lineNo++) {
+      System.out.println("enter a line to input");
+      String userInput = fileNameInput.nextLine();
+      fileOutput.println(userInput);
+    }
 
-    
   }
+
 }
